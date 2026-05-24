9074a3a330b30bc0ee891d6eab9728a06abc9414
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-05-24 21:47:30 +0200

    Implement Role schema model class

---
+++ src/main/java/app/model/Role.java
@@ -0,0 +1,24 @@
+package app.model;
+
+/* Role class representing the roles table from the DB.
+ * Fields:
+ * - id: int (primary key, auto-incremented)
+ * - name: String (unique, not null)
+ * */
+public class Role {
+    private int id;
+    private String name;
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+}
