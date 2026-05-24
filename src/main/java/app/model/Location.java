5c1434ef9539a5ee7180029b76a55f7bab30b332
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-05-24 22:00:54 +0200

    Implement Location schema model class

---
+++ src/main/java/app/model/Location.java
@@ -0,0 +1,44 @@
+package app.model;
+
+/* Location class representing the locations table in the database.
+ * This class represents a physical location. */
+public class Location {
+    private int id;
+    private String city;
+    private String postalCode;
+    private String district;
+    private String streetAddress;
+    private String country;
+
+    public Location(int id, String city, String postalCode, String district, String streetAddress, String country) {
+        this.id = id;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.district = district;
+        this.streetAddress = streetAddress;
+        this.country = country;
+    }
+
+    public Location(String city, String postalCode, String district, String streetAddress, String country) {
+        this.city = city;
+        this.postalCode = postalCode;
+        this.district = district;
+        this.streetAddress = streetAddress;
+        this.country = country;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getCity() { return city; }
+    public String getPostalCode() { return postalCode; }
+    public String getDistrict() { return district; }
+    public String getStreetAddress() { return streetAddress; }
+    public String getCountry() { return country; }
+
+    // Setters
+    public void setCity(String city) { this.city = city; }
+    public void setPostalCode(String postalCode) { this.postalCode = postalCode; }
+    public void setDistrict(String district) { this.district = district; }
+    public void setStreetAddress(String streetAddress) { this.streetAddress = streetAddress; }
+    public void setCountry(String country) { this.country = country; }
+}
