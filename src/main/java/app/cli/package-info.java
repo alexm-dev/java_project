33d08e0f2fe3693e389425ba28528094d145acf3
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-05-26 18:26:47 +0200

    Add javadoc to package-info app.cli

---
+++ src/main/java/app/cli/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains the CLI components.
+ * The AdminMenu class is the admin dashboard that allows administrators to manage users, view reports, and perform other administrative tasks.
+ *
+ * The TerminalApp is a temporary CLI testbed to test the functionality of ShareSpace before migration into the JavaFX UI.
+ */
+
+package app.cli;
