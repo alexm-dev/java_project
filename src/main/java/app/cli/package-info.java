0242bd5879bd0a77d242f450def0bba669d88ff3
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-05-26 20:17:36 +0200

    Update app.cli package info javadoc

--- src/main/java/app/cli/package-info.java
+++ src/main/java/app/cli/package-info.java
@@ -1,6 +1,6 @@
 /**
- * This package contains the CLI components.
- * The AdminMenu class is the admin dashboard that allows administrators to manage users, view reports, and perform other administrative tasks.
+ * This package contains the temporary CLI components.
+ * The AdminMenu class is a temporary admin dashboard that allows administrators to manage users, view reports, and perform other administrative tasks.
  *
  * The TerminalApp is a temporary CLI testbed to test the functionality of ShareSpace before migration into the JavaFX UI.
  */
